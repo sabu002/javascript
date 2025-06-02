9b5d71057ae5fa82fea608973cf849f00b3ef316
sabu002 <khanalsabita072@gmail.com>
2025-06-02 17:54:22 +0545

    add test

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ console.log("hello world")
 console.log("happy me")
 let number= [1,3,5,6]
 console.log(number[2])
+let person={
+    name:"me",
+    age:45,
+    address:"ktm"
+}
+console.log(person.name)
